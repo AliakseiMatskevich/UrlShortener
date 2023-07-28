4657a9673d8e6ee45bf198313eafccdf86cfda46
Алексей Мацкевич <aliaksei.matskevich@gmail.com>
2023-07-28 11:30:59 +0300

    Add entities, DbContext, initial migration, IRepository, Repository

---
+++ src/UrlShortener.Infrastructure/Data/EFRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrlShortener.ApplicationCore.Entities.Abstracts;
+using UrlShortener.ApplicationCore.Interfaces;
+
+namespace UrlShortener.Infrastructure.Data
+{
+    public class EFRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly UrlShortenerContext _context;
+        public EFRepository(UrlShortenerContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<T> CreateAsync(T entity)
+        {
+            _context.Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public IQueryable<T> GetList()
+        {
+            return _context.Set<T>();
+        }
+    }
+}
---
+++ src/UrlShortener.Infrastructure/Data/UrlShortenerContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrlShortener.ApplicationCore.Entities;
+
+namespace UrlShortener.Infrastructure.Data
+{
+    public class UrlShortenerContext : DbContext
+    {
+        public DbSet<Url> Urls { get; set; }
+
+        public UrlShortenerContext(DbContextOptions<UrlShortenerContext> options) : base(options) { }        
+    }
+}
